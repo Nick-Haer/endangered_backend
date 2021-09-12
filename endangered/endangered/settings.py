8198faee23c01061a30357f84513263e91d3f4c6
Nick.Haer <nhaer123@gmail.com>
2021-09-12 02:08:23 -0500

    Add heroku to allowed hosts

--- endangered/endangered/settings.py
+++ endangered/endangered/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l=)+6z+@cvb&)isuyi07jzzj%xts9@k8u-74vt3==^9nwyb3-)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
