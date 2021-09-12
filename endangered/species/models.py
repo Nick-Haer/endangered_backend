ea1540dea7c34cb4485d0aa9e9245d17c5736674
Nick.Haer <nhaer123@gmail.com>
2021-09-11 20:21:42 -0500

    add models for animals, and data gathering requests, admin setup

--- endangered/species/models.py
+++ endangered/species/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Animal(models.Model):
+    name = models.CharField(max_length=200)
+    latin_name = models.CharField(max_length=200, default="none")
+    status = models.CharField(max_length=200, default="none")
+    image_url = models.CharField(max_length=200, default="none")
+    description = models.CharField(max_length=10000, default="none")
+
+    def __str__(self):
+        return self.name
--- endangered/species/views.py
+++ endangered/species/views.py
@@ -1,12 +1,84 @@
 from django.shortcuts import render
 
 import requests
+from bs4 import BeautifulSoup
 
 from django.http import HttpResponse
+
+from .models import Animal
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def getWikiData():
+    animals = Animal.objects.all()
+
+    for animal in animals:
+        wikiReadyAnimalName = animal.name.capitalize().replace(" ", "_")
+        wikiUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&titles={}&prop=pageimages|extracts&format=json&pithumbsize=100&exsentences=10&exlimit=1&explaintext=1".format(
+            wikiReadyAnimalName)
+        wikiPage = requests.get(wikiUrl).json()
+        print(wikiPage['query']["pages"])
+        if (type(wikiPage['query']["pages"]) is dict):
+            if ('thumbnail' in list(wikiPage['query']["pages"].values())[0]):
+
+                print(list(wikiPage['query']["pages"].values())[
+                    0]["thumbnail"]["source"])
+
+                animal.image_url = list(wikiPage['query']["pages"].values())[
+                    0]["thumbnail"]["source"]
+            if ('extract' in list(wikiPage['query']["pages"].values())[0]):
+
+                print(list(wikiPage['query']["pages"].values())[
+                    0]["extract"])
+
+                animal.description = list(wikiPage['query']["pages"].values())[
+                    0]["extract"]
+            animal.save()
+            print(animal.description)
+
+
+def getMatchingCharities():
+    queryString = "https://api.data.charitynavigator.org/v2/Organizations?app_id=d7b095ba&app_key=9f6820071629919514265b3843172891"
+    PARAMS = {'app_id': "d7b095ba",
+              "app_key": "9f6820071629919514265b3843172891"}
+    charities = requests.get(queryString, PARAMS)
+
+
+def getWWFAnimalsData():
+    page = requests.get(
+        'https://www.worldwildlife.org/species/directory')
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    table = soup.table
+
+    rows = table.find_all("tr")
+
+    for row in rows:
+        name = row.a.contents[0]
+        latin_name = ''
+        status = ''
+
+        if (row.find("em") and len(row.find("em")) != 0):
+            latin_name = row.em.contents[0]
+        else:
+            latin_name = "none"
+
+        tableCellsFound = (row.find_all("td") and len(row.find_all("td")) != 0)
+
+        if (tableCellsFound and row.find_all("td")[2].text):
+            status = row.find_all("td")[2].text
+        else:
+            status = "none"
+
+        print(bool(status))
+
+        a = Animal(name=name, latin_name=latin_name, status=status)
+        a.save()
+        print(a.id)
 
 
 def detail(request, question_id):
